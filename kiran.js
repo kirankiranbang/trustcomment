cedfcd76ac01d5d06067469d42c949b9142e1b1a
kiran B <kirankiranbang@gmail.com>
2023-04-14 11:42:59 +0530

    html commit1

--- kiran.js
+++ kiran.js
@@ -1,3 +1,11 @@
-console.log("hi");
-console.log("bye");
-console.log("i love india");
+var headerTitle=document.getElementById('header-title')
+headerTitle.style.borderBottom="solid 3px #000";
+
+
+
+
+
+//JavaScript syntax:	object.style.fontStyle="italic"//
+
+
+//https://www.w3schools.com/cssref/pr_font_font-style.php//
