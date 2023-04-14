dcfe73e618843570d1dd49af54b7f30c18ad8ea4
kiran B <kirankiranbang@gmail.com>
2023-04-14 12:25:00 +0530

    js commit

--- kiran.js
+++ kiran.js
@@ -5,7 +5,7 @@ headerTitle.style.borderBottom="solid 3px #000";
 
 
 
-//JavaScript syntax:	object.style.fontStyle="italic"//
+//JavaScript syntax:	object.style.fontStyle="italic"
 
 
 //https://www.w3schools.com/cssref/pr_font_font-style.php//
